c1de76a1c190d6ced54d7720fc51750dbb51941c
yarokojo <passtyyaro302@gmail.com>
2026-07-14 07:14:48 +0000

    fix: Remove duplicate path and multer declarations in index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
-const path = require("path");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
